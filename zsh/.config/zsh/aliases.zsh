cf89fb9cc11ae6d5d7d99a1a05ea2ef3d251aa41
Keto <therealketo@gmail.com>
2023-08-22 22:20:00 -0400

    zsh: Let's do aliases differently...

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -4,12 +4,12 @@
 # to is installed, or exists in any way, shape, or form.
 create_alias() {
     if command -v "$2" &>/dev/null; then
-        alias "$1=$2 $3"
+        alias "$1=$3"
     fi
 }
 
-create_alias "ls" "lsd" "-a --icon never"
-create_alias "bat" "bat" "--theme \"TwoDark\""
+create_alias "ls" "lsd" "lsd -a -icon never"
+create_alias "bat" "bat" "bat --theme \"TwoDark\""
 
 # Always use GNU make on macOS
-create_alias "make" "gmake"
+create_alias "make" "gmake" "gmake"
